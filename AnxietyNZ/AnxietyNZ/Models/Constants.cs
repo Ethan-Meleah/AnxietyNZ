b2ec1533fc7d7aa3b7b0c23f44aea144efe167d5
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-15 08:39:46 +1200

    Untested Styling + Logo
    For Login Page

--- AnxietyNZ/AnxietyNZ/Models/Constants.cs
+++ AnxietyNZ/AnxietyNZ/Models/Constants.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AnxietyNZ.Models
 {
     class Constants
     {
         public static bool IsDev = true;
+
+        public static Color BackgroundColor = Color.FromRgb(179, 204, 255);
+        public static Color MainTextColor = Color.White;
+
+        public static int LoginIconHeight = 120;
     }
 }
--- AnxietyNZ/AnxietyNZ/Views/LoginPage.xaml.cs
+++ AnxietyNZ/AnxietyNZ/Views/LoginPage.xaml.cs
@@ -16,6 +16,20 @@ namespace AnxietyNZ.Views
         public LoginPage()
         {
             InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            BackgroundColor = Constants.BackgroundColor;
+            Lbl_Username.TextColor = Constants.MainTextColor;
+            Lbl_Password.TextColor = Constants.MainTextColor;
+
+            ActivitySpinner.IsVisable = false;
+            LoginIcon.HeightRequest = Constants.LoginIconHeight;
+
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
         }
 
         void SignInProcedure(object sender, EventArgs e)
@@ -23,11 +37,11 @@ namespace AnxietyNZ.Views
             User user = new User(Entry_Username.Text, Entry_Password.Text);
             if (user.CheckInformation())
             {
-                DisplayAlert("Login", "Login Success", "Oke");
+                DisplayAlert("Login", "Login Success", "Ok");
             }
             else
             {
-                DisplayAlert("Login", "Login incorrect, empty username or password", "Oke");
+                DisplayAlert("Login", "Login incorrect, empty username or password", "Ok");
             }
         }
     }
